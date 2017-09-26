e2f99ba107ef34625e83fb74ae5f7e8635eb3ae1
Leif Åstrand <leif@vaadin.com>
2017-09-26 14:32:49 +0300

    Load polyfill to ensure we wait for web components to load

--- trippy-starter/src/main/java/com/vaadin/trippy/impl/Servlet.java
+++ trippy-starter/src/main/java/com/vaadin/trippy/impl/Servlet.java
@@ -26,7 +26,8 @@ import com.vaadin.server.VaadinServletConfiguration;
  */
 @WebServlet(urlPatterns = "/*", name = "UIServlet", asyncSupported = true, initParams = {
         // Use "new" default for the frontend location
-        @WebInitParam(name = "frontend.url.es6", value = "context://frontend/") })
+        @WebInitParam(name = "frontend.url.es6", value = "context://frontend/"),
+        @WebInitParam(name = "polyfillBase", value = "frontend://bower_components/webcomponentsjs/") })
 @VaadinServletConfiguration(productionMode = false, usingNewRouting = true)
 public class Servlet extends VaadinServlet {
 }
