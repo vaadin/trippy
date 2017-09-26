3e46395c89f05c784312d978a9967b87f5f1c3dd
Leif Åstrand <leif@vaadin.com>
2017-09-26 15:39:50 +0300

    Add DatePicker and use with Binder

--- trippy-ui/src/main/java/com/vaadin/trippy/TripEditor.java
+++ trippy-ui/src/main/java/com/vaadin/trippy/TripEditor.java
@@ -2,23 +2,38 @@ package com.vaadin.trippy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.vaadin.data.Binder;
 import com.vaadin.router.HasUrlParameter;
 import com.vaadin.router.Route;
 import com.vaadin.router.event.BeforeNavigationEvent;
 import com.vaadin.trippy.data.Trip;
 import com.vaadin.trippy.data.TripRepository;
+import com.vaadin.ui.datepicker.DatePicker;
 import com.vaadin.ui.html.Div;
+import com.vaadin.ui.html.NativeButton;
 
 @Route("trip")
 public class TripEditor extends Div implements HasUrlParameter<String> {
     @Autowired
     private TripRepository tripRepository;
 
+    private Binder<Trip> binder = new Binder<>();
+
+    public TripEditor() {
+        DatePicker datePicker = new DatePicker();
+
+        binder.bind(datePicker, Trip::getDate, Trip::setDate);
+
+        add(datePicker, new NativeButton("Save", e -> {
+            tripRepository.save(binder.getBean());
+        }));
+    }
+
     @Override
     public void setParameter(BeforeNavigationEvent event, String parameter) {
         Trip trip = tripRepository.findOne(new Long(parameter));
 
-        setText(trip.getFormattedDate());
+        binder.setBean(trip);
     }
 
 }
