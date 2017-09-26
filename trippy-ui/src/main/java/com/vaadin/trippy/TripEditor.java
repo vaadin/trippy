d632c5e56b16403516d624af6b5175b24d3fc325
Leif Åstrand <leif@vaadin.com>
2017-09-26 18:56:34 +0300

    Add a distance field and some validation logic

--- trippy-ui/src/main/java/com/vaadin/trippy/TripEditor.java
+++ trippy-ui/src/main/java/com/vaadin/trippy/TripEditor.java
@@ -3,6 +3,7 @@ package com.vaadin.trippy;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.vaadin.data.Binder;
+import com.vaadin.data.converter.StringToDoubleConverter;
 import com.vaadin.router.HasUrlParameter;
 import com.vaadin.router.Route;
 import com.vaadin.router.event.BeforeNavigationEvent;
@@ -11,6 +12,7 @@ import com.vaadin.trippy.data.TripRepository;
 import com.vaadin.ui.datepicker.DatePicker;
 import com.vaadin.ui.html.Div;
 import com.vaadin.ui.html.NativeButton;
+import com.vaadin.ui.textfield.TextField;
 
 @Route("trip")
 public class TripEditor extends Div implements HasUrlParameter<String> {
@@ -21,12 +23,30 @@ public class TripEditor extends Div implements HasUrlParameter<String> {
 
     public TripEditor() {
         DatePicker datePicker = new DatePicker();
+        TextField distanceField = new TextField();
+        distanceField.setPreventInvalidInput(true);
+        distanceField.setPattern("[.0-9]*");
 
-        binder.bind(datePicker, Trip::getDate, Trip::setDate);
+        binder.forField(datePicker).asRequired("Please select a date")
+                .bind(Trip::getDate, Trip::setDate);
+        binder.forField(distanceField)
+                .withConverter(
+                        new StringToDoubleConverter(0d, "Must enter a number"))
+                .asRequired("Must enter a number")
+                .bind(Trip::getLength, Trip::setLength);
 
-        add(datePicker, new NativeButton("Save", e -> {
-            tripRepository.save(binder.getBean());
-        }));
+        NativeButton submitButton = new NativeButton("Save", e -> {
+            if (binder.isValid()) {
+                tripRepository.save(binder.getBean());
+            }
+        });
+
+        binder.addStatusChangeListener(event -> {
+            submitButton.getElement().setAttribute("disabled",
+                    event.hasValidationErrors());
+        });
+
+        add(datePicker, distanceField, submitButton);
     }
 
     @Override
