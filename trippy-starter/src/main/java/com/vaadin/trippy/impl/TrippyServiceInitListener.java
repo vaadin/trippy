91ea5ced1d89efb4f4b48a1b0c4b82e3ef43eab8
Leif Åstrand <leif@vaadin.com>
2017-09-30 20:26:31 +0300

    Add crude Google Maps functionality

--- trippy-starter/src/main/java/com/vaadin/trippy/impl/TrippyServiceInitListener.java
+++ trippy-starter/src/main/java/com/vaadin/trippy/impl/TrippyServiceInitListener.java
@@ -1,9 +1,14 @@
 package com.vaadin.trippy.impl;
 
+import org.jsoup.nodes.Element;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
 import com.vaadin.server.BootstrapListener;
 import com.vaadin.server.BootstrapPageResponse;
 import com.vaadin.server.ServiceInitEvent;
 import com.vaadin.server.VaadinServiceInitListener;
+import com.vaadin.server.VaadinServletService;
 
 /**
  * Various hacks that should really be features in Flow instead
@@ -17,12 +22,32 @@ public class TrippyServiceInitListener implements VaadinServiceInitListener {
             event.addDependencyFilter(dependencyFilter);
         }
 
+        VaadinServletService service = (VaadinServletService) event.getSource();
+
+        WebApplicationContext applicationContext = WebApplicationContextUtils
+                .findWebApplicationContext(
+                        service.getServlet().getServletContext());
+
+        String apiKey = applicationContext.getEnvironment()
+                .getProperty("map.apikey");
+        if (apiKey == null) {
+            throw new RuntimeException(
+                    "Configure a map.apikey in your application.properties");
+        }
+
         event.addBootstrapListener(new BootstrapListener() {
             @Override
             public void modifyBootstrapPage(BootstrapPageResponse response) {
-                response.getDocument().head().appendElement("meta")
-                        .attr("name", "viewport")
+                Element head = response.getDocument().head();
+                head.appendElement("meta").attr("name", "viewport")
                         .attr("content", "width=device-width, initial-scale=1");
+
+                // Flow sets element properties too late for google-map to get
+                // the right API key. As a temporary workaround, we put our key
+                // in the map's prototype instead.
+                head.appendElement("script").html(
+                        "customElements.whenDefined('google-map').then(function() {customElements.get('google-map').prototype.apiKey = '"
+                                + apiKey + "'})");
             }
         });
     }
