82038660fe2041f057cf54f7732ea4c2a53ec1ad
Leif Åstrand <leif@vaadin.com>
2017-10-04 18:44:18 +0300

    Simplify trip adding code and remove unused views

--- trippy-ui/src/main/java/com/vaadin/trippy/AddTrip.java
+++ trippy-ui/src/main/java/com/vaadin/trippy/AddTrip.java
@@ -1,24 +1,67 @@
 package com.vaadin.trippy;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.vaadin.data.Binder;
+import com.vaadin.flow.model.TemplateModel;
 import com.vaadin.router.ParentLayout;
 import com.vaadin.router.Route;
 import com.vaadin.trippy.data.Trip;
 import com.vaadin.trippy.data.TripRepository;
+import com.vaadin.ui.Tag;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.button.Button;
+import com.vaadin.ui.common.HasStyle;
+import com.vaadin.ui.common.HtmlImport;
+import com.vaadin.ui.datepicker.DatePicker;
+import com.vaadin.ui.polymertemplate.Id;
+import com.vaadin.ui.polymertemplate.PolymerTemplate;
+import com.vaadin.ui.textfield.TextField;
 
 @Route("add")
 @ParentLayout(MainLayout.class)
-public class AddTrip extends TripForm {
+@Tag("trip-form")
+@HtmlImport("TripForm.html")
+public class AddTrip extends PolymerTemplate<TemplateModel>
+        implements HasStyle {
     @Autowired
     private TripRepository tripRepository;
 
-    @PostConstruct
-    private void init() {
-        DirectionSearch.getCurrent().showRoute("", "");
+    @Id("date")
+    private DatePicker datePicker;
+    @Id("from")
+    private TextField from;
+    @Id("to")
+    private TextField to;
+    @Id("save")
+    private Button saveButton;
+
+    public AddTrip() {
         setClassName("add-trip");
-        edit(new Trip(), tripRepository::saveAndFlush);
+
+        Binder<Trip> binder = new Binder<>();
+
+        binder.forField(datePicker).bind(Trip::getDate, Trip::setDate);
+        binder.forField(from).bind(Trip::getStart, Trip::setStart);
+        binder.forField(to).bind(Trip::getEnd, Trip::setEnd);
+
+        binder.addValueChangeListener(e -> {
+            DirectionSearch.getCurrent().previewRoute(from.getValue(),
+                    to.getValue());
+        });
+
+        saveButton.addClickListener(e -> {
+            Trip trip = new Trip();
+            if (binder.writeBeanIfValid(trip)) {
+                tripRepository.saveAndFlush(trip);
+
+                UI ui = UI.getCurrent();
+                String url = ui.getRouter().get().getUrl(TripList.class,
+                        trip.getId());
+                ui.navigateTo(url);
+            }
+        });
+
+        DirectionSearch.getCurrent().showRoute("", "");
     }
 }
